20ca750dd72dd17306acb06fc2ed56f71c85b83a
vannoordenne <45230262+vannoordenne@users.noreply.github.com>
2021-12-01 15:08:53 +0100

    uploaded files

---
+++ server.js
@@ -0,0 +1,37 @@
+const http = require('http');
+const fs = require('fs');
+
+http.createServer(function (req, res) {
+
+    if (req.method === 'POST') {
+        // console.log("POST");
+        // let body = '';
+        req.on('data', function (data) {
+            //body += data;
+            //console.log("Partial body: " + body);
+
+            fs.writeFile('api_res.json', data, err => {
+                if (err) {
+                    console.log('Error writing file', err)
+                } else {
+                    console.log('Successfully wrote file')
+                }
+            });
+        });
+
+        res.writeHead(200, {'Content-Type': 'text/plain','Access-Control-Allow-Origin': '*'});
+        res.end('{"msg": "OK"}'); // removed the callback because it was not relevant in the client code
+
+        // // unsure if this is needed
+        // req.on('end', function () {
+        //    console.log("Body: " + body);// + body
+        // });
+    }
+    else {
+        //console.log("GET");
+        res.writeHead(200, {'Content-Type': 'text/plain', 'Access-Control-Allow-Origin': '*'});
+        res.end('{"msg": "OK"}'); // removed the callback because it was not relevant in the client code
+    }
+
+}).listen(8090);
+console.log('Server running on port 8090');
