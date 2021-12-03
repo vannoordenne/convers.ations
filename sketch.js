1ec75b7aeaca9c130d6c1a35734668312270444d
vannoordenne <45230262+vannoordenne@users.noreply.github.com>
2021-12-03 12:22:32 +0100

    including p5js library

--- sketch.js
+++ sketch.js
@@ -1,16 +1,34 @@
-let json;
-let SRA = [];
-let transcript = "Hello World!";
+// let json;
+// let SRA = [];
+// let transcript;
+var counter = 1;
 
 function setup(){
     createCanvas(1680, 920);
-    background(0);
+    loadData();
+    setInterval(loadData, 100);
 }
 
-function draw(){
-    //json = loadJSON(api_res.json);
-   // transcript = json.SpeechRecognitionAlternative[0].transcript;
+// function draw(){
+//     setInterval(loadData, 1000);
+//     //drawTranscript(transcript);
+//     //json = loadJSON(api_res.json);
+//    // transcript = json.SpeechRecognitionAlternative[0].transcript;
+// }
+
+function loadData(){
+    loadJSON('api_res.json', drawTranscript);
+    // transcript = json.SpeechRecognitionAlternative[0].transcript;
+    // drawTranscript(transcript);
+    // console.log(transcript);
+}
+
+function drawTranscript(json){
+    var transcript = json.SpeechRecognitionAlternative[0].transcript;
+    background(0);
     textAlign(CENTER);
+    textSize(32);
     fill(255);
     text(transcript, width/2, height/2);
+    counter++;
 }
