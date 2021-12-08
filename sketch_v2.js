50a13ffe4a60a18ede640d833ff37a32a860a680
vannoordenne <45230262+vannoordenne@users.noreply.github.com>
2021-12-08 19:16:05 +0100

    including p5js library

--- sketch_v2.js
+++ sketch_v2.js
@@ -1,7 +1,9 @@
 var counter = 1;
 
 function setup(){
+    frameRate(1);
     createCanvas(1680, 920);
+    background(0);
     loadData();
     setInterval(loadData, 100);
 }
@@ -12,11 +14,23 @@ function loadData(){
 
 function drawTranscript(json){
     var transcript = json.SpeechRecognitionAlternative;
-    background(0);
+    //background(0);
     textAlign(CENTER);
     textSize(32);
     fill(255);
-    //text(transcript, width/2, height/2);
+    if(transcript[0].isFinal == true){
+        background(255);
+        fill(0);
+        textSize(64);
+        text(transcript[0].transcript, width/2, height/2 - 32);
+        background(0);
+    }
+    else{
+        fill(255);
+        textSize(32);
+        text(transcript[0].transcript, random(0, width), random(0, height));
+    }
+    //for(let i = 0; i < transcript.length; )
     console.log(transcript);
     counter++;
 }
